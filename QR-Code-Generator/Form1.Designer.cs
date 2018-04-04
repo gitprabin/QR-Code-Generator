a7e380217fa0ef4bf8919446965ab416dee34642
prabinsiwakoti <developer.prabin@gmail.com>
2018-04-04 20:05:10 +0545

    Icon added

--- QR-Code-Generator/Form1.Designer.cs
+++ QR-Code-Generator/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.txtPlainText = new System.Windows.Forms.RichTextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
@@ -117,6 +118,7 @@
             this.Controls.Add(this.txtPlainText);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "QR-Code-Generator";
