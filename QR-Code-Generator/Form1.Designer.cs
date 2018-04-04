cebd76a3f255c851ee42b1b9699537cabc67fc44
prabinsiwakoti <developer.prabin@gmail.com>
2018-04-04 19:55:43 +0545

    Repository created and basic implementation.

--- QR-Code-Generator/Form1.Designer.cs
+++ QR-Code-Generator/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPlainText = new System.Windows.Forms.RichTextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Text";
+            // 
+            // txtPlainText
+            // 
+            this.txtPlainText.Location = new System.Drawing.Point(24, 49);
+            this.txtPlainText.Name = "txtPlainText";
+            this.txtPlainText.Size = new System.Drawing.Size(207, 174);
+            this.txtPlainText.TabIndex = 1;
+            this.txtPlainText.Text = "";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(24, 244);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(207, 23);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate QR Code";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(24, 273);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(207, 23);
+            this.btnDecode.TabIndex = 2;
+            this.btnDecode.Text = "Decode QR Code";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(25, 302);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(207, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "Browse a Local Image";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(264, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(185, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(264, 259);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(185, 23);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(475, 333);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtPlainText);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR-Code-Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtPlainText;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnDownload;
     }
 }
 
--- QR-Code-Generator/Form1.cs
+++ QR-Code-Generator/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing.Common;
+using ZXing;
+using ZXing.QrCode;
+using System.Drawing;
 
 namespace QR_Code_Generator
 {
@@ -15,6 +12,90 @@ namespace QR_Code_Generator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        QrCodeEncodingOptions options = new QrCodeEncodingOptions();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 250,
+                Height = 250,
+            };
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtPlainText.Text) || String.IsNullOrEmpty(txtPlainText.Text))
+            {
+                pictureBox1.Image = null;
+                MessageBox.Show("Text not found", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var qr = new BarcodeWriter();
+                qr.Options = options;
+                qr.Format = BarcodeFormat.QR_CODE;
+                var result = new Bitmap(qr.Write(txtPlainText.Text.Trim()));
+                pictureBox1.Image = result;
+                txtPlainText.Clear();
+            }
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Bitmap bitmap = new Bitmap(pictureBox1.Image);
+                BarcodeReader reader = new BarcodeReader { AutoRotate = true, TryInverted = true };
+                Result result = reader.Decode(bitmap);
+                string decoded = result.ToString().Trim();
+                txtPlainText.Text = decoded;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Image not found", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                var qr = new BarcodeWriter();
+                qr.Options = options;
+                qr.Format = BarcodeFormat.QR_CODE;
+                pictureBox1.ImageLocation = open.FileName;
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("Image not found", "Oops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                SaveFileDialog save = new SaveFileDialog();
+                save.CreatePrompt = true;
+                save.OverwritePrompt = true;
+                save.FileName = "QR";
+                save.Filter = "PNG|*.png|JPEG|*.jpg|BMP|*.bmp|GIF|*.gif";
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image.Save(save.FileName);
+                    save.InitialDirectory = Environment.GetFolderPath
+                                (Environment.SpecialFolder.Desktop);
+                }
+            }
         }
     }
 }
